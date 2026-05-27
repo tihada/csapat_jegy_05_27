1b944eb9833ee0d8fdf1acf0152f47897b9f9395
tihada <c2tihada@vasvari.org>
2026-05-27 09:04:23 +0200

    UI + default menu items

--- Form1.cs
+++ Form1.cs
@@ -13,5 +13,19 @@ namespace csapat_jegy_05_27 {
         public Form1() {
             InitializeComponent();
         }
+
+        private void kilépToolStripMenuItem_Click(object sender, EventArgs e) {
+            if (MessageBox.Show("Biztosan kilép?", "Kilépés", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes) {
+                Application.Exit();
+            }
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e) {
+            MessageBox.Show("Tihanyi Ádám - Vörös Dávid\n2026.06.27", "Információk", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void beolvasHandler(object sender, EventArgs e) {
+
+        }
     }
 }
