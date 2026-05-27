c4b302b413239d030c6aab78bd561637b30be5b4
VorosDavidVasvari <12345678+VorosDavidVasvari@users.noreply.github.com>
2026-05-27 09:10:50 +0200

    fitler setup

--- Form1.cs
+++ Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,6 +13,31 @@ namespace csapat_jegy_05_27 {
     public partial class Form1 : Form {
         public Form1() {
             InitializeComponent();
+        }
+
+        public void onFilterTxtBoxChange(object sender, EventArgs e) {
+            var filterTxtBox = sender as TextBox;
+            string filter = filterTxtBox.Text;
+
+
+            string lbName = $"{filterTxtBox.Name.Replace("_textBox", "")}_listBox";
+            ListBox lb = (GetListBoxByName(lbName) as ListBox);
+        
+            lb.Items.Clear();
+
+            foreach (var item in lb.Items) {
+                if (Regex.IsMatch(item.ToString(), filter)) {
+                    lb.Items.Add(item);
+                }
+            }
+        }
+
+        public object GetListBoxByName(string listBoxName) {
+            var listBox = this.Controls.Find(listBoxName, true).FirstOrDefault() as ListBox;
+            if (listBox != null) {
+                return listBox;
+            }
+            return new object[0];
         }
     }
 }
